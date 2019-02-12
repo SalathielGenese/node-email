05c3e4d6a9caf5d9b05d868617f793bd8940c9f7
SalathielGenese <salathielgenese@gmail.com>
2019-02-12 06:47:00 +0100

    email

---
+++ src/email.js
@@ -0,0 +1,16 @@
+const email = async ( transport, { to, from, html, subject }) =>
+{
+    return new Promise( ( resolve, reject ) =>
+    {
+        transport.sendMail(
+            { to, from, html, subject },
+            ( error, info ) => error ? reject( error ) : resolve( info )
+        );
+    });
+}
+
+
+
+module.exports = {
+    email,
+};
